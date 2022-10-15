abd7c167411df084cf93c1462548cb7a6804e10e
borisovski-borche <borisovski.borce@gmail.com>
2022-10-15 09:53:39 +0200

    updated token duration

--- api/const/jwt.const.js
+++ api/const/jwt.const.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const createAccessToken = userId => {
   return jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "3s",
+    expiresIn: "20m",
   });
 };
 
